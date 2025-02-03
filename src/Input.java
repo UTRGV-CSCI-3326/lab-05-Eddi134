33411243c3e2afa8829e7374b720d3ab449513fc
Eddi134 <edgar.skywalker@hotmail.com>
2025-02-03 03:03:53 +0000

    first commit

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,25 @@
+public class MedicalForm {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Enter your weight (kg): ");
+        double weight = scanner.nextDouble();
+
+        System.out.println("Are you a smoker? (true/false)");
+        boolean isSmoker = scanner.nextBoolean();
+
+        System.out.println("\nMedical Form Summary: ");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight + " kg");
+        System.out.println("Smoker: " + isSmoker);
+
+        scanner.close();
+    }
+}
